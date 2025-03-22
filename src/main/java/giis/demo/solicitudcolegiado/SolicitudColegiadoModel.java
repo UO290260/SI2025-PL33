30d30c07ce04fb2ed314060478fc24b157add5e1
aleja <aleja@Lalo>
2025-03-22 19:41:02 +0100

    Historia 90% , comprobar si hay algun error mas y si falta algo documentado

--- src/main/java/giis/demo/solicitudcolegiado/SolicitudColegiadoModel.java
+++ src/main/java/giis/demo/solicitudcolegiado/SolicitudColegiadoModel.java
@@ -1,18 +1,35 @@
 package giis.demo.solicitudcolegiado;
-
 import java.util.List;
-
 import giis.demo.util.Database;
 
 public class SolicitudColegiadoModel {
-
 	private Database db = new Database();
-	
 
+	/**
+	 * Obtener lista de colegiados pendientes
+	 * @return
+	 */
 	public List<ColegiadoDTO> getListaPendientes() {
-		String sql = "SELECT id_colegiado, nombre, apellidos, DNI, direccion, poblacion, fecha_nacimiento, cuenta_bancaria, titulacion, fecha_colegiacion, estado "
-				+ "FROM Colegiados WHERE estado = 'Pendiente'";
+		String sql = "SELECT id_colegiado, nombre, apellidos, DNI, titulacion, estado FROM Colegiados WHERE estado = 'Pendiente'";
 		return db.executeQueryPojo(ColegiadoDTO.class, sql);
 	}
-}
 
+	/**
+	 * Obtener lista de colegiados enviados
+	 * @return
+	 */
+	public List<ColegiadoDTO> getListaColegiadosEnviados() {
+		String sql = "SELECT id_colegiado, nombre, apellidos, DNI, titulacion, estado FROM Colegiados WHERE estado = 'Enviado'";
+		return db.executeQueryPojo(ColegiadoDTO.class, sql);
+	}
+
+	/**
+	 * Actualizar el estado del colegiado
+	 * @param colegiado
+	 * @param estado
+	 */
+	public void actualizarEstadoColegiado(ColegiadoDTO colegiado, String estado) {
+		String sql = "UPDATE Colegiados SET estado = ? WHERE DNI = ?";
+		db.executeUpdate(sql, estado, colegiado.getDNI());
+	}
+}
