d183d370bef30ea7b2d83d52443795b2f100f3b0
Pablo <Pablo@DESKTOP-1SEJN4N>
2025-03-24 16:55:25 +0100

    eliminación de lineas en blanco

--- src/main/java/giis/demo/inscripcioncursos/InscripcionCursosController.java
+++ src/main/java/giis/demo/inscripcioncursos/InscripcionCursosController.java
@@ -1,7 +1,6 @@
 package giis.demo.inscripcioncursos;
 
 import java.awt.event.ActionEvent;
-
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
