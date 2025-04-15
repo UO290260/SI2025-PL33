a96f9f1434c3fbbf7445036c9396f21aa4510268
Alejandro <UO294431@uniovi.es>
2025-04-15 13:35:10 +0200

    Historia funcional (detalle por resolver)

--- src/main/java/giis/demo/visualizarcursos/VisualizarCursosView.java
+++ src/main/java/giis/demo/visualizarcursos/VisualizarCursosView.java
@@ -1,8 +1,10 @@
 package giis.demo.visualizarcursos;
 
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.SystemColor;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,8 +17,10 @@ import net.miginfocom.swing.MigLayout;
 public class VisualizarCursosView {
 	
 	private JFrame frame;
-	private JTable tabla;
+	private JTable tablaCursos, tablaInscritos;
 	private JComboBox<String> boxColectivos;
+	private JButton boton;
+	private JPanel panelBoton;
 	
 	public VisualizarCursosView() {
 		initialize();
@@ -29,7 +33,7 @@ public class VisualizarCursosView {
 		frame = new JFrame();
 		frame.setTitle("Visualizar Cursos");
 		frame.setName("Visualizar Cursos");
-		frame.setBounds(0, 0, 1400, 550);
+		frame.setBounds(0, 0, 1000, 550);
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(new MigLayout("", "[grow]", "[][][grow][][][][][][][][]"));
@@ -47,21 +51,38 @@ public class VisualizarCursosView {
 		boxColectivos = new JComboBox<>(colectivos);
 		panel.add(boxColectivos);
 				
-		
 		//Tabla para mostrar los cursos
-		tabla = new JTable();
-		tabla.setName("tablaCursos");
-		tabla.setRowSelectionAllowed(false);
-		tabla.setDefaultEditor(Object.class, null); 
-		tabla.setBackground(SystemColor.control);
-		JScrollPane tableDetallePanel = new JScrollPane(tabla);
-		frame.getContentPane().add(tableDetallePanel, "cell 0 13 2 1, grow, wrap");
+		tablaCursos = new JTable();
+		tablaCursos.setName("tablaCursos");
+		tablaCursos.setRowSelectionAllowed(false);
+		tablaCursos.setRowSelectionAllowed(true);
+		tablaCursos.setDefaultEditor(Object.class, null);
+		tablaCursos.setBackground(SystemColor.control);
+		JScrollPane panelCursos = new JScrollPane(tablaCursos);
+		frame.getContentPane().add(panelCursos, "cell 0 13 2 1, grow, wrap");
 		
+		//Tabla para mostrar los inscritos a un curso
+		tablaInscritos = new JTable();
+		tablaInscritos.setName("tablaInscritos");
+		tablaInscritos.setRowSelectionAllowed(false);
+		tablaInscritos.setDefaultEditor(Object.class, null); 
+		tablaInscritos.setBackground(SystemColor.control);
+		JScrollPane panelInscritos = new JScrollPane(tablaInscritos);
+		frame.getContentPane().add(panelInscritos, "grow");
+		
+		//Boton para cancelar un curso
+		panelBoton = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		boton = new JButton("Cancelar Curso");
+		boton.setBounds(0, 0, 130, 23);
+		panelBoton.add(boton);
+		frame.getContentPane().add(panelBoton);
 	}
 	
 	public JFrame getFrame() { return frame; }
-	public JTable getTabla() { return tabla; }
+	public JTable getTablaCursos() { return tablaCursos; }
+	public JTable getTablaInscritos() { return tablaInscritos; }
 	public JComboBox<String> getOpcion() { return boxColectivos; }
+	public JButton getBoton() { return boton; }
 
 }
 
